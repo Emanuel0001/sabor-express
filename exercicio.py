aaf74a093c8f31ecc5994ee3391200ca6ca7bc02
Emanuel <hitallosoares1@gmail.com>
2024-07-08 13:13:53 -0300

    primeiro exercicio e app inicio

---
+++ exercicio.py
@@ -0,0 +1,16 @@
+print('Python na Escola de Programação da Alura')
+nome = 'hitallo'
+idade = 26
+print(f'meu nome é {nome} e minha idade é {idade} ')
+print('meu nome é {} e tenho {} anos de idade'.format(nome,idade))
+print(""" 
+      A
+      L
+      U
+      R
+      A
+      """)
+
+pi = 3.14159
+
+print(f'o valor aproximado de pi é {pi:.2f}')
